265d60dd0297b0e82b68e11b174ea56aae6fdaa9
Loic <llh1@sanger.ac.uk>
2013-12-03 11:47:02 +0000

    Ability to update a gel image metadata by gel uuid

--- lib/lims-quality-app/gel-image-metadata/update_gel_image_metadata.rb
+++ lib/lims-quality-app/gel-image-metadata/update_gel_image_metadata.rb
@@ -5,12 +5,22 @@ module Lims::QualityApp
   class GelImageMetadata
     class UpdateGelImageMetadata
       include Lims::Core::Actions::Action
-      attribute :gel_image_metadata, GelImageMetadata, :required => true
+      attribute :gel_image_metadata, GelImageMetadata, :required => false
+      attribute :gel_uuid, String, :required => false
       attribute :score, String, :required => true
+      validates_with_method :ensure_gel_parameter
 
       def _call_in_session(session)
-        gel_image_metadata.score = score
-        {:gel_image_metadata => gel_image_metadata}
+        metadata = gel_image_metadata || session.gel_image_metadata[:gel_uuid => gel_uuid]
+        metadata.score = score
+        {:gel_image_metadata => metadata}
+      end
+
+      def ensure_gel_parameter
+        unless gel_image_metadata || gel_uuid
+          return [false, "A gel image metadata uuid or a gel uuid must be passed."]
+        end
+        true
       end
     end
 
