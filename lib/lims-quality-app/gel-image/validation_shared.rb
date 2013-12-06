23c9d09590b931ebfc00961561a0d59d88ae2d18
Loic <llh1@sanger.ac.uk>
2013-12-06 15:56:43 +0000

    Refactoring

--- lib/lims-quality-app/gel-image/validation_shared.rb
+++ lib/lims-quality-app/gel-image/validation_shared.rb
@@ -6,7 +6,7 @@ module Lims::QualityApp
       def ensure_scores
         scores.each do |location, score|
           unless GelImage::Score::SCORES.include?(score.to_s.downcase)
-            return [false, "#{score} is not a valid score for the location #{location}. Valid scores are #{GelImage::Score::SCORES.inspect}"]
+            return [false, "#{score} is not a valid score for the location #{location}. Valid scores are #{GelImage::Score::SCORES.join(", ")}."]
           end
         end
         true
