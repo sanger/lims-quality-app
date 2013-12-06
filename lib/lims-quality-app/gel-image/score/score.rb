54dbbc6094a61a46f3fdb5d3838ad8a230803d09
Loic <llh1@sanger.ac.uk>
2013-12-06 16:07:40 +0000

    Do not display gel filter and score resources in the root json

--- lib/lims-quality-app/gel-image/score/score.rb
+++ lib/lims-quality-app/gel-image/score/score.rb
@@ -5,6 +5,7 @@ module Lims::QualityApp
   class GelImage
     class Score
       include Lims::Core::Resource
+      NOT_IN_ROOT = 1
 
       SCORES = ["pass", "fail", "degraded", "partially degraded"]
       attribute :score, String, :required => true, :initializable => true
