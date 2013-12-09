a07bf46ba1f3274b6e20f4066668870c69ea6b87
Loic <llh1@sanger.ac.uk>
2013-12-09 10:29:29 +0000

    Add position validation

--- lib/lims-quality-app/gel-image/validation_shared.rb
+++ lib/lims-quality-app/gel-image/validation_shared.rb
@@ -3,10 +3,16 @@ require 'lims-quality-app/gel-image/score/score'
 module Lims::QualityApp
   class GelImage
     module ValidationShared
+      VALID_POSITION = /^[A-Za-z][0-9]+$/ 
+
       def ensure_scores
-        scores.each do |location, score|
+        scores.each do |position, score|
+          unless position =~ VALID_POSITION
+            return [false, "#{position} is not a valid position."]
+          end
+
           unless GelImage::Score::SCORES.include?(score.to_s.downcase)
-            return [false, "#{score} is not a valid score for the location #{location}. Valid scores are #{GelImage::Score::SCORES.join(", ")}."]
+            return [false, "#{score} is not a valid score for the position #{position}. Valid scores are #{GelImage::Score::SCORES.join(", ")}."]
           end
         end
         true
